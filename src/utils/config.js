d91f2dcafb59a9140f640877ad0433cfb212b1b3
Nikita Titov <netitov@gmail.com>
2022-10-25 13:39:13 +0300

    актуализирована ссылка на апи

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const MAIN_API = "http://localhost:3001";
-export const DEV_API = "http://localhost:8080";
+export const MAIN_API = "https://api.table.netitov.ru/";
+export const DEV_API = "http://localhost:3001";
 
 
